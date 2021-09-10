9483ea672391a3467c846f8dfdbea9ef81ebf098
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-09-09 22:22:17 -0600

    Create all primary color and typography

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
 import store from './redux/configureStore';
 
 ReactDOM.render(
