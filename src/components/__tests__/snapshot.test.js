a57fe8b4369c606d191fe80f8ab6ed20d1891ba1
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-09-10 17:16:21 -0600

    Completed all snapshots test for all main components and routes

---
+++ src/components/__tests__/snapshot.test.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import App from '../../App';
+import store from '../../redux/configureStore';
+import Header from '../../components/Header/Header';
+import GniWorld from '../../components/GniWorld/GniWorld';
+import Details from '../../pages/Details';
+
+describe('App', () => {
+  it('Renders the Homepage', () => {
+    const app = TestRenderer.create(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    ).toJSON();
+    expect(app).toMatchSnapshot();
+  });
+});
+
+describe('Header', () => {
+  it('Renders the Header', () => {
+    const initialPath = {
+      path: '/',
+      groupId: '',
+      currentCategory: {
+        current: 'region',
+        other: 'income',
+      },
+    };
+
+    const header = TestRenderer.create(
+        <Header currentPath={initialPath} />
+    ).toJSON();
+    expect(header).toMatchSnapshot();
+  });
+});
+
+describe('GniWorld', () => {
+  it('Renders the homepage GniWorld', () => {
+    const pathX = '/';
+
+    const func = (newPath) => {
+      if (pathX !== newPath) {
+        setPath(() => newPath);
+      }
+    };
+
+    const current = {
+      currentCategory: {
+        current: 'region',
+        other: 'income',
+      },
+    };
+
+    const gniWorld = TestRenderer.create(
+      <Provider store={store}>
+        <GniWorld
+          updatePath={func}
+          currentCategory={current}
+        />
+      </Provider>,
+    ).toJSON();
+    expect(gniWorld).toMatchSnapshot();
+  });
+});
+
+describe('Details', () => {
+  it('Renders the Details page', () => {
+    const groupId = 'EAS';
+    const pathX = '/';
+
+    const details = TestRenderer.create(
+      <Provider store={store}>
+        <Details
+          groupId={groupId}
+          currentPath={pathX}
+        />
+      </Provider>,
+    ).toJSON();
+    expect(details).toMatchSnapshot();
+  });
+});
