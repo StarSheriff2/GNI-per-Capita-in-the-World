25d2fa37145409ed370ba5f9b01e780742a51685
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-09-10 19:46:30 -0600

    Create tests for getGniWorldSuccess actiion

--- src/components/__tests__/unitTests.test.js
+++ src/components/__tests__/unitTests.test.js
@@ -1,8 +1,9 @@
-import reducer, { getDetailsSuccess, getDetailsFailed } from '../../redux/details/details';
+import detailsReducer, { getDetailsSuccess, getDetailsFailed } from '../../redux/details/details';
+import gniWorldReducer, { getGniWorldSuccess, getGniWorldFailed } from '../../redux/gniWorld/gniWorld';
 
 describe('detailsReducer', () => {
   test('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(
+    expect(detailsReducer(undefined, {})).toEqual(
       {
         status: 'idle',
         entities: {},
@@ -67,7 +68,7 @@ describe('detailsReducer', () => {
       ],
     };
 
-    expect(reducer(previousState, getDetailsSuccess(newGroup))).toEqual(
+    expect(detailsReducer(previousState, getDetailsSuccess(newGroup))).toEqual(
       {
         entities: {
           EAS: [
@@ -89,10 +90,128 @@ describe('detailsReducer', () => {
 
     const errorMessage = 'error message';
 
-    expect(reducer(previousState, getDetailsFailed(errorMessage))).toEqual({
+    expect(detailsReducer(previousState, getDetailsFailed(errorMessage))).toEqual({
       entities: {},
       error: 'error message',
       status: 'failed',
     });
   });
+});
+
+describe('gniWorldReducer', () => {
+  test('should return the initial state', () => {
+    expect(gniWorldReducer(undefined, {})).toEqual(
+      {
+        status: 'idle',
+        entities: [],
+      },
+    );
+  });
+
+  test('should handle payload from successful API call', () => {
+    const previousState = {
+      status: 'idle',
+      entities: [],
+    };
+
+    const apiPayload = [
+      {
+        indicator: {
+          id: 'NY.GNP.PCAP.CD',
+          value: 'GNI per capita, Atlas method (current US$)',
+        },
+        country: {
+          id: 'XD',
+          value: 'High income',
+        },
+        countryiso3code: '',
+        date: '2020',
+        value: 46036.0466787998,
+        unit: '',
+        obs_status: '',
+        decimal: 0,
+      },
+      {
+        indicator: {
+          id: 'NY.GNP.PCAP.CD',
+          value: 'GNI per capita, Atlas method (current US$)',
+        },
+        country: {
+          id: 'XM',
+          value: 'Low income',
+        },
+        countryiso3code: '',
+        date: '2020',
+        value: 810.751972755762,
+        unit: '',
+        obs_status: '',
+        decimal: 0,
+      },
+      {
+        indicator: {
+          id: 'NY.GNP.PCAP.CD',
+          value: 'GNI per capita, Atlas method (current US$)',
+        },
+        country: {
+          id: 'XN',
+          value: 'Lower middle income',
+        },
+        countryiso3code: '',
+        date: '2020',
+        value: 2200.71995911578,
+        unit: '',
+        obs_status: '',
+        decimal: 0,
+      },
+      {
+        indicator: {
+          id: 'NY.GNP.PCAP.CD',
+          value: 'GNI per capita, Atlas method (current US$)',
+        },
+        country: {
+          id: 'XT',
+          value: 'Upper middle income',
+        },
+        countryiso3code: '',
+        date: '2020',
+        value: 9425.53004618163,
+        unit: '',
+        obs_status: '',
+        decimal: 0,
+      },
+    ];
+
+    expect(gniWorldReducer(previousState, getGniWorldSuccess(apiPayload))).toEqual(
+      {
+        entities: [{
+          name: 'High income', indicator: 46036.0466787998, id: 'XD', date: '2020', category: 'income',
+        },
+        {
+          name: 'Low income', indicator: 810.751972755762, id: 'XM', date: '2020', category: 'income',
+        },
+        {
+          name: 'Lower middle income', indicator: 2200.71995911578, id: 'XN', date: '2020', category: 'income',
+        },
+        {
+          name: 'Upper middle income', indicator: 9425.53004618163, id: 'XT', date: '2020', category: 'income',
+        }],
+        status: 'idle',
+      },
+    );
+  });
+/*
+  test('should handle error', () => {
+    const previousState = {
+      status: 'idle',
+      entities: {},
+    };
+
+    const errorMessage = 'error message';
+
+    expect(gniWorldReducer(previousState, getDetailsFailed(errorMessage))).toEqual({
+      entities: {},
+      error: 'error message',
+      status: 'failed',
+    });
+  }); */
 });
