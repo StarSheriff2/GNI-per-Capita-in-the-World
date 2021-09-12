586ba3cec6a90946c248f931cec89bfce9653c33
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-09-11 20:28:26 -0600

    Completed all userInteraction / Integration tests

--- src/__tests__/userInteraction.test.js
+++ src/__tests__/userInteraction.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import LoadAnimation from '../components/LoadAnimation/LoadAnimation';
@@ -12,6 +12,10 @@ import gniWorldReducer from '../mocks/gniReducer';
 import detailsReducer from '../mocks/detailsReducer';
 
 afterEach(cleanup);
+
+beforeEach(() => {
+  jest.resetModules();
+});
 
 const reducer = combineReducers({
   gniWorld: gniWorldReducer,
@@ -23,156 +27,137 @@ const renderWithRedux = (component,
   ...render(<Provider store={store}>{component}</Provider>),
 });
 
-test('Shows correct hidden text', () => {
-  const { getByTestId } = renderWithRedux(<LoadAnimation />);
-  expect(getByTestId('spinnerSpan').textContent).toBe('Loading...');
-});
-
-test('Shows correct title when in homepage', () => {
-  const initialPath = {
-    path: '/',
-    groupId: '',
-    currentCategory: {
-      current: 'region',
-      other: 'income',
-    },
-  };
-
-  const { getByTestId } = renderWithRedux(<Header currentPath={initialPath} />);
-  expect(getByTestId('headerText').textContent).toBe('gni per capita in the world');
-});
-
-test('Shows correct title', () => {
-  const setCategoryFilter = jest.fn();
-
-  const changeCategoryFilter = () => setCategoryFilter((actualCategory) => ({
-    current: actualCategory.other,
-    other: actualCategory.current,
-  }));
-
-  const props = {
-    currentCategory: 'region',
-    otherCategory: 'income',
-    changeCategoryFilter,
-  };
-
-  const { getByTestId } = renderWithRedux(<Filter
-    currentCategory={props.currentCategory}
-    otherCategory={props.otherCategory}
-    changeCategoryFilter={props.changeCategoryFilter}
-  />);
-
-  expect(getByTestId('currentCategory').textContent).toBe('REGIONS');
-});
-
-test('Show group regions in homepage on first render of app', async () => {
-  const { findByText } = renderWithRedux(<App />);
-  expect(await findByText(/^north america$/i)).toBeInTheDocument();
-});
-
-test('Change category title when user clicks filter button', async () => {
-  const { getByTestId, findByRole } = renderWithRedux(<App />);
-
-  expect(getByTestId('spinnerSpan').textContent).toBe('Loading...');
-
-  userEvent.click(await findByRole('button'));
-  expect(getByTestId('currentCategory').textContent).toBe('INCOME LEVELS');
-  expect(getByTestId('currentCategory').textContent).not.toBe('REGIONS');
-});
-
-test('Should briefly show Spinner when status of state is \'starting\'', async () => {
-  const { getByTestId } = renderWithRedux(<App />);
-
-  expect(getByTestId('spinnerSpan').textContent).toBe('Loading...');
-});
-
-test('When user clicks a region in the homepage, the app renders the corrsponding details page', async () => {
-  const { findByText } = renderWithRedux(<App />);
-
-  expect(await findByText(/^south asia$/i)).toBeInTheDocument();
-  userEvent.click(await findByText(/^south asia$/i));
-  // expect(findByText(/^india$/i)).toBe('India');
-  expect(await findByText(/^india$/i)).toBeInTheDocument();
-  // expect(getByTestId('currentCategory').textContent).not.toBe('REGIONS');
-});
-
-/* describe('Header', () => {
-  it('Shows proper header', () => {
-    const app = TestRenderer.create(
-      <Provider store={store}>
-        <App />
-      </Provider>,
-    ).toJSON();
-
-    fireEvent.click(screen.getByTestId('linkCalc'));
-
-    const title = screen.getByTestId('header');
-    expect(title.textContent).toBe('gni per capita in the world');
+describe('Hompage', () => {
+  test('Shows correct hidden text', () => {
+    const { getByTestId } = renderWithRedux(<LoadAnimation />);
+    expect(getByTestId('spinnerSpan').textContent).toBe('Loading...');
   });
 
-  it('Renders home page', () => {
-    render(<App />);
+  test('Shows correct title when in homepage', () => {
+    let initialPath = {
+      path: '/',
+      groupId: '',
+      currentCategory: {
+        current: 'region',
+        other: 'income',
+      },
+    };
 
-    fireEvent.click(screen.getByTestId('linkHome'));
+    const pathX = '/';
 
-    const title = screen.getByTestId('homeTitle');
-    expect(title.textContent).toBe('Welcome To Our Page!');
+    const setPath = (newPath) => initialPath = newPath;
+
+    const updatePath = (newPath) => {
+      if (pathX !== newPath) {
+        setPath(newPath);
+      }
+    };
+
+    const { getByTestId } = renderWithRedux(<Header currentPath={initialPath} updatePath={updatePath} />);
+    expect(getByTestId('headerText').textContent).toBe('gni per capita in the world');
   });
 
-  it('Renders quote page', () => {
-    render(<App />);
+  test('Shows correct title', () => {
+    const setCategoryFilter = jest.fn();
 
-    fireEvent.click(screen.getByTestId('linkQuote'));
+    const changeCategoryFilter = () => setCategoryFilter((actualCategory) => ({
+      current: actualCategory.other,
+      other: actualCategory.current,
+    }));
 
-    const title = screen.getByTestId('quoteTitle');
-    expect(title.textContent).toBe('Quote of the day:');
+    const props = {
+      currentCategory: 'region',
+      otherCategory: 'income',
+      changeCategoryFilter,
+    };
+
+    const { getByTestId } = renderWithRedux(<Filter
+      currentCategory={props.currentCategory}
+      otherCategory={props.otherCategory}
+      changeCategoryFilter={props.changeCategoryFilter}
+    />);
+
+    expect(getByTestId('currentCategory').textContent).toBe('REGIONS');
+  });
+
+  test('Show group regions on first render of app', async () => {
+    const { findByText } = renderWithRedux(<App />);
+    expect(await findByText(/^north america$/i)).toBeInTheDocument();
+  });
+
+  test('Change category title when user clicks filter button', async () => {
+    const { getByTestId, findByRole } = renderWithRedux(<App />);
+
+    expect(getByTestId('spinnerSpan').textContent).toBe('Loading...');
+
+    userEvent.click(await findByRole('button'));
+    expect(getByTestId('currentCategory').textContent).toBe('INCOME LEVELS');
+    expect(getByTestId('currentCategory').textContent).not.toBe('REGIONS');
+  });
+
+  test('Should briefly show Spinner when status of state is \'starting\'', async () => {
+    const { getByTestId } = renderWithRedux(<App />);
+
+    expect(getByTestId('spinnerSpan').textContent).toBe('Loading...');
+  });
+
+  test('When user clicks a region area, the app renders the corrsponding details page', async () => {
+    const { findByText } = renderWithRedux(<App />);
+
+    expect(await findByText(/^south asia$/i)).toBeInTheDocument();
+    userEvent.click(await findByText(/^south asia$/i));
+    expect(await findByText(/^india$/i)).toBeInTheDocument();
+    expect(await findByText(/^pakistan$/i)).toBeInTheDocument();
+    const aus = screen.queryByText(/^australia$/i);
+    expect(aus).not.toBeInTheDocument();
+    const bhu = screen.queryByText(/^bhutan$/i);
+    expect(bhu).toBeInTheDocument();
+    const arrowBtn = screen.queryByText(/^<$/i);
+    userEvent.click(arrowBtn);
   });
 });
 
-describe('CalculatorPage', () => {
-  it('displays a number', () => {
-    render(<CalculatorPage />);
+describe('Details page by current group', () => {
+  test('When user clicks the back arrow, app goes back to homepage', async () => {
+    let initialPath = {
+      path: '/',
+      groupId: '',
+      currentCategory: {
+        current: 'region',
+        other: 'income',
+      },
+    };
 
-    fireEvent.click(screen.getByRole('button', { name: '0' }));
+    const { findByText, getByTestId } = renderWithRedux(<App />, applyMiddleware(thunk), {
+      initialState: {
+        gniWorld: {
+          status: 'idle',
+          entities: [
+            {name: "East Asia & Pacific", indicator: 11706.2486723881, id: "EAS", date: "2020", category: "region"},
+            {name: "Europe & Central Asia", indicator: 25212.5210325623, id: "ECS", date: "2020", category: "region"},
+            {name: "High income", indicator: 46036.0466787998, id: "XD", date: "2020", category: "income"},
+            {name: "Latin America & Caribbean", indicator: 7715.11850464998, id: "LCN", date: "2020", category: "region"},
+            {name: "Low income", indicator: 810.751972755762, id: "XM", date: "2020", category: "income"},
+            {name: "Lower middle income", indicator: 2200.71995911578, id: "XN", date: "2020", category: "income"},
+            {name: "Middle East & North Africa", indicator: 6917.78109613348, id: "MEA", date: "2020", category: "region"},
+            {name: "North America", indicator: 63921.605596663, id: "NAC", date: "2020", category: "region"},
+            {name: "South Asia", indicator: 1820.60030063242, id: "SAS", date: "2020", category: "region"},
+            {name: "Sub-Saharan Africa", indicator: 1478.56962278708, id: "SSF", date: "2020", category: "region"},
+            {name: "Upper middle income", indicator: 9425.53004618163, id: "XT", date: "2020", category: "income"},
+          ],
+        },
+        details: {
+          status: 'idle',
+          entities: {},
+        }
+      }
+    });
 
-    const display = screen.getByTestId('display');
-    expect(display.textContent).toBe('  0');
+    userEvent.click(await findByText(/^south asia$/i));
+    expect(await findByText(/^india$/i)).toBeInTheDocument();
+    const arrowBtn = screen.queryByText(/^<$/i);
+    userEvent.click(arrowBtn);
+    expect(await findByText(/^north america$/i)).toBeInTheDocument();
+    expect(getByTestId('currentCategory').textContent).toBe('REGIONS');
   });
-
-  it('displays a number', () => {
-    render(<CalculatorPage />);
-
-    fireEvent.click(screen.getByRole('button', { name: '4' }));
-    fireEvent.click(screen.getByRole('button', { name: '+' }));
-    fireEvent.click(screen.getByRole('button', { name: '8' }));
-    fireEvent.click(screen.getByRole('button', { name: '=' }));
-
-    const display = screen.getByTestId('display');
-    expect(display.textContent).toBe('12 + ');
-  });
-
-  it('displays a number', () => {
-    render(<CalculatorPage />);
-
-    fireEvent.click(screen.getByRole('button', { name: '1' }));
-    fireEvent.click(screen.getByRole('button', { name: '0' }));
-    fireEvent.click(screen.getByRole('button', { name: '-' }));
-    fireEvent.click(screen.getByRole('button', { name: '5' }));
-    fireEvent.click(screen.getByRole('button', { name: '=' }));
-
-    const display = screen.getByTestId('display');
-    expect(display.textContent).toBe('5 - ');
-  });
-
-  it('displays a number', () => {
-    render(<CalculatorPage />);
-
-    fireEvent.click(screen.getByRole('button', { name: '8' }));
-    fireEvent.click(screen.getByRole('button', { name: '÷' }));
-    fireEvent.click(screen.getByRole('button', { name: '2' }));
-    fireEvent.click(screen.getByRole('button', { name: '=' }));
-
-    const display = screen.getByTestId('display');
-    expect(display.textContent).toBe('4 ÷ ');
-  });
-}); */
+});
